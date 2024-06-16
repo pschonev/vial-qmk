141569f55a5affe1583f8f11a0d75ffc0a7e9872
adesso-philipp-schoneville <p.schoneville@gmx.net>
2024-06-16 14:23:04 +0200

    chore: Add working config

---
+++ keyboards/sofle_choc/keymaps/pschonev/config.h
@@ -0,0 +1,29 @@
+/* Copyright 2023 Brian Low
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define SPLIT_USB_DETECT
+
+// Vial Support
+#define VIAL_KEYBOARD_UID \
+    { 0xA7, 0xA7, 0x9C, 0x4C, 0xE0, 0xB8, 0x00, 0x8D }
+
+// The four corners
+#define VIAL_UNLOCK_COMBO_ROWS \
+    { 0, 5, 3, 8 }
+#define VIAL_UNLOCK_COMBO_COLS \
+    { 0, 0, 0, 0 }
